99e31209cbc69d3cfb2a501919f1d4d7fa8dda0b
Anshul <anshulbadhale23@gmail.com>
2023-12-11 17:12:20 +0530

    mynotes-api

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb+srv://anshull23:AntulBantai%4023011606@cluster0.azwepr3.mongodb.net/notes');
+
+const NotesSchema = new mongoose.Schema({
+    note : {
+        title: String,
+        content: String}
+});
+
+const NotesModel = mongoose.model("notes", NotesSchema);
+
+app.post('/add', (req, res) => {
+    const note = req.body.note;
+    NotesModel.create({
+        note: note
+    }).then(result => res.json(result))
+    .catch(err => res.json(err))
+});
+
+app.get('/get', (req,res) => {
+    NotesModel.find()
+    .then(result => res.json(result))
+    .catch(err => res.json(err))
+})
+
+app.delete('/delete/:id', (req,res) => {
+    const {id} = req.params;
+    NotesModel.findByIdAndDelete({_id: id})
+    .then(result => res.json(result))
+    .catch(err => res.json(err))
+})
+
+app.listen(8000, () => {
+    console.log("Server is Running");
+});
